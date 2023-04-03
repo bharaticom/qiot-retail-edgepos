47a20bd78d4cedc2a0f3e07217d0dbceb65a9d10
panchakarlabk <bharat_icom@icomsol.co.uk>
2023-04-03 09:38:49 +0100

    update new changes

---
+++ src/main/java/org/drools/hackfest2022/Products.java
@@ -0,0 +1,13 @@
+package org.drools.hackfest2023;
+
+public class Products {
+    public String id;
+    public String name;
+    public String price;
+
+    public Products (String id, String name, String price){
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    } 
+}
